36c802a2f97ed9cd0e3c7b5b60dbeadc9e4e8af7
gitee_com_zb <18749379836@163.com>
2023-02-23 20:07:05 +0800

    测试

---
+++ test_2_23/test.cpp
@@ -0,0 +1,85 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	int a = 10;
+//	cout << "Enter value:>";
+//	cin >> a;
+//	cout << "hello C++" << endl << "a=" << a << endl;
+//	return 0;
+//}
+
+//void Fun2()
+//{
+//	int b = 0;
+//	printf("%p\n", &b);
+//}
+//
+//void Fun1()
+//{
+//	int a = 0;
+//	printf("%p\n", &a);
+//}
+//
+//int main()
+//{
+//	Fun1();
+//	Fun2();
+//
+//	return 0;
+//}
+
+//int Fun()
+//{
+//	int a = 10;
+//	return a;
+//}
+//
+//int main()
+//{
+//	int ret=Fun();
+//	
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int year = 0;
+//	int flag = 0; 
+//	while (scanf("%d", &year))
+//	{
+//		if ((year % 4 == 0) && (year % 100 == 0) || (year % 400 == 0))
+//			flag = 1;//是闺年
+//		else
+//			flag = 0;//不是闰年
+//
+//
+//		if (1 == flag)
+//			printf("%d is a leap year!\n", year);
+//		else
+//			printf("%d is not a leap year!\n", year);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
